bb4a63880d29c0a417aa33972d58e64bd3271447
Zoe <zoeeunicedelos@shgcham.school.nz>
2026-05-04 09:10:51 +1200

    Added template files

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    index.html
+pass
+
+if __name__ == "__main__":
+    app.run(debug=True);
