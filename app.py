4f78be0cde980207281e79440e16c3f661651205
Zoe <zoeeunicedelos@shgcham.school.nz>
2026-05-21 12:58:27 +1200

    Improved cards, updated dev log

--- app.py
+++ app.py
@@ -3,9 +3,29 @@ from livereload import server
 
 app = Flask(__name__)
 
+def get_pages():
+    pages = [
+        {
+            'link_href': 'abcdef',
+            'img_src': 'images/smiley.jpg',
+            'img_alt': 'smiley face',
+            'title': 'ABC',
+            'summary': 'abcdefg',
+        },
+        {
+            'link_href': 'abcdef',
+            'img_src': 'images/smiley.jpg',
+            'img_alt': 'smiley face',
+            'title': 'ABC',
+            'summary': 'abcdefg',
+        }
+    ]
+    return pages
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    pages = get_pages()
+    return render_template("index.html", pages=pages)
 
 @app.route('/login')
 def login():
